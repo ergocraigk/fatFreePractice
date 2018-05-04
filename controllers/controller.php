428786da8028241dac302189761c20e9f1c81163
Craig Koch <ergocraigk@gmail.com>
2018-05-03 18:27:34 -0700

    Added some routes for displaying prime numbers

--- controllers/controller.php
+++ controllers/controller.php
@@ -17,4 +17,24 @@
         //show the message
         echo Template::instance()->render('views/specificSaying.php');
     }
+
+    function printHomeTown($fatFree, $town){
+        //snag a variable from the router
+        $fatFree->set('town', $town);
+
+        //display the home town given
+        echo Template::instance()->render('views/homeTown.php');
+    }
+
+    function printPrimes($fatFree, $low, $high){
+        $primes = findPrimes($low, $high);
+
+        //save our data to the router
+        $fatFree->set('primes', $primes);
+
+        //load the page
+        echo Template::instance()->render('views/primes.php');
+    }
+
+
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
     //global includes
     require_once 'controllers/controller.php';
+    require_once 'model/model.php';
 
     //load the fat free framework
     $fatFree = require 'vendor/bcosca/fatfree-core/base.php';
@@ -23,6 +24,14 @@
 
     $fatFree->route('GET /saying/@message', function($fatFree, $params){
         printSpecificSaying($fatFree, $params['message']);
+    });
+
+    $fatFree->route('GET /HomeTown/@town', function($fatFree, $params){
+        printHomeTown($fatFree, $params['town']);
+    });
+
+    $fatFree->route('GET /primes/@low/@high', function($fatFree, $params){
+        printPrimes($fatFree, $params['low'], $params['high']);
     });
 
     $fatFree->run();
